89092926cad98e36c9db67f5488e474b6a7ee2b8
WingsJ0 <wings_j@outlook.com>
2019-10-30 22:08:13 +0800

    代码完成

--- code/extension.js
+++ code/extension.js
@@ -1,8 +1,12 @@
 const Vscode = require('vscode');
-const GetRelativePath=require('./src/index')
+const GetRelativePath = require('./src/get-relative-path')
 
-function activate(context) {
-    context.subscriptions.push(Vscode.commands.registerCommand('extension.GetRelativePath', GetRelativePath));
+function activate(context)
+{
+    context.subscriptions.push(Vscode.commands.registerCommand('extension.getRelativePath', (target) =>
+    {
+        GetRelativePath(target)
+    }));
 }
 
 exports.activate = activate;
